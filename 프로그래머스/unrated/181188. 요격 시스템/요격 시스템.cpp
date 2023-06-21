24950ef0b8d4ceef26abcfb89f54dff1f995c057
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2023-06-22 00:57:41 +0900

    [unrated] Title: 요격 시스템, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+bool Cmp(vector<int> A, vector<int> B){
+    return A[0] < B[0];
+    //시작 구간 기준 오름차순 정렬
+}
+
+
+int solution(vector<vector<int>> targets) {
+    sort(targets.begin(), targets.end(), Cmp);
+    
+    priority_queue<int, vector<int>, greater<int>> PQ;
+    
+    PQ.push(targets[0][1]);
+    int Cnt = 1;
+    for(int i = 1; i < targets.size(); i++)
+    {
+        int First = targets[i][0];
+        int Last = targets[i][1];
+        
+        if(PQ.top() <= First)
+        {
+            Cnt++;
+            while(!PQ.empty()) PQ.pop();
+            PQ.push(Last);
+        }
+        else
+        {
+            PQ.push(Last);
+        }
+        
+    }
+    
+    return Cnt;
+}
