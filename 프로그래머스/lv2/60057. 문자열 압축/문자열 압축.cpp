8faca0f86a5e9f46078abff09098cc1418e099bd
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2023-08-18 20:30:05 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.cpp
@@ -0,0 +1,93 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <iostream>
+using namespace std;
+
+int Ans = 9999;
+
+void Check_Zip(string s, int count)
+{
+    
+    string Fin = "";
+    
+    
+    string Temp = s.substr(0, count);
+    string Curr = "";
+    
+    int Same = 0;
+    
+    int en = 0;
+    for(int st = 0; st < s.length(); st++)
+    {
+        while(en - st < count && en < s.length())
+        {
+            Curr += s[en];
+            en++;
+        }
+        //en - st = count 상태
+        
+        if(Curr == Temp)
+        {
+            if(en != count) Same++;
+        }
+        else
+        {
+            if(Same > 0)
+            {
+                string Insert = to_string(Same + 1) + Temp;
+                Fin += Insert;
+                Same = 0;
+            }
+            else if(Same == 0)
+            {
+                Fin += Temp;
+            }
+            
+            // cout << "next string : " << Curr << endl;
+            Temp = Curr;
+        }
+        
+        Curr = "";
+        st = en - 1;
+        
+    }
+    // 마지막 문자열 검증 진행
+    
+    if(Curr == Temp)
+    {
+        if(en != count) Same++;
+    }
+    else
+    {
+        if(Same > 0)
+        {
+            string Insert = to_string(Same + 1) + Temp;
+            Fin += Insert;
+            Same = 0;
+        }
+        else if(Same == 0)
+        {
+            Fin += Temp;
+        }
+            
+        // cout << "next string : " << Curr << endl;
+        Temp = Curr;
+    }
+
+    
+    
+    // cout << "Fin : " << Fin << endl << endl;
+    Ans = min(Ans, static_cast<int>(Fin.length()));
+}
+
+
+int solution(string s) {
+    for(int i = 0; i < s.length(); i++)
+    {
+        Check_Zip(s, i + 1);
+    }
+    return Ans;
+}
