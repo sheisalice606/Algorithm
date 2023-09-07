867adc732f477c1e6641907e0581f4aef125aa7f
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2023-09-08 00:16:08 +0900

    [level 3] Title: 순위, Time: 10.29 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.cpp
+++ 프로그래머스/lv3/49191. 순위/순위.cpp
@@ -20,18 +20,21 @@ int Check_Count(int n) {
     return cnt - 1;
 }
 
+void Clear() {
+    memset(Visit, 0, sizeof(Visit));
+    memset(Check, 0, sizeof(Check));
+}
+
 void Winner_Count(int n) {
     for(int i = 0; i < Win[n].size(); i++)
     {
         int next = Win[n][i];
         if(Visit[n][next] == 1) continue; 
         
-        //방문 체크 & 승부 추가
         Visit[n][next] = 1;
         Winner_Count(next);
     }
     
-    //종단 노드
     Check[n] = 1;
 }
 
@@ -41,7 +44,6 @@ void Loser_Count(int n) {
         int next = Lose[n][i];
         if(Visit[n][next] == 1) continue;
         
-        //방문 체크 & 승부 추가
         Visit[n][next] = 1;
         Loser_Count(next);
     }
@@ -59,30 +61,23 @@ int solution(int n, vector<vector<int>> results) {
         int lose = v[1];
         
         Win[win].push_back(lose);
-        //win 이 lose 를 이겼다.
         Lose[lose].push_back(win);
-        //lose 가 win 에게 졌다.
     }
     
     for(int i = 1; i <= n; i++)
     {        
         Winner_Count(i);
         int cnt_A = Check_Count(n);
-        
-        memset(Visit, 0, sizeof(Visit));
-        memset(Check, 0, sizeof(Check));
+        Clear();
         
         Loser_Count(i);
         int cnt_B = Check_Count(n);
-        
-        memset(Visit, 0, sizeof(Visit));
-        memset(Check, 0, sizeof(Check));
+        Clear();
         
         if(cnt_A + cnt_B == n - 1) 
         {
             Ans++;
         }
-        
     }
    
     return Ans;
