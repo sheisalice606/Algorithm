9c7e69f94e5251b90fa9ee3855fc2989ed2349b7
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2023-09-29 19:01:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+int dp[1001];
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= N; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+	cout << dp[N];
+}
