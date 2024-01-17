ea42fca2de60b9d23396760cffc89c717570741f
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-01-17 21:21:12 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    if(A > B) cout << '>';
+    else if(A < B) cout << '<';
+    else cout << "==";
+}
