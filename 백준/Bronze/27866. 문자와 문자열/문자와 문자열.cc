46dd7f8d8c76c9e2559a4bbbd6a64ae82ce5b6e7
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-01-20 23:45:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    string s; 
+    
+    cin >> s;
+    cin >> n;
+    
+    cout << s[n-1];
+    
+}
