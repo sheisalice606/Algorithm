c0f8b0901e4fb744b4dce3f56ba3b88ca95901ff
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-18 22:05:15 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 24 ms, Memory: 2592 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.cc
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.cc
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <queue>
 #include <cstring>
+#include <string>
 using namespace std;
 
 int N;
@@ -9,21 +11,25 @@ bool Flag = false;
 
 struct Room {
 	char type;
-	int cost;
 	vector<int> next;
+	int cost;
 };
+
 Room Map[1001];
 int Visit[1001];
 
 
 void Input() {
 	cin >> N;
-	if (N == 0) exit(0);
-	for (int i = 1; i <= N; i++)
-	{
+	if (N == 0) {
+		exit(0);
+	}
+
+	//입력
+	for (int i = 1; i <= N; i++) {
 		Room room;
 		cin >> room.type >> room.cost;
-		while (true) 
+		while (true)
 		{
 			int next; cin >> next;
 			if (next == 0) break;
@@ -33,36 +39,25 @@ void Input() {
 	}
 }
 
-bool Check(int curr, int cost, int next) {
-	char type = Map[next].type;
-	if (type == 'T' && cost - Map[next].cost < 0) return false;
-	return true;
-}
 
 void Dfs(int curr, int cost) {
 
-	if (curr == N)
-	{
+	if (curr == N) {
 		Flag = true;
 		return;
 	}
 
-	if (Map[curr].type == 'L' && cost <= Map[curr].cost)
-	{
-		cost = Map[curr].cost;
-	}
+	if (Map[curr].type == 'T') cost -= Map[curr].cost;
+	if (Map[curr].type == 'L' && cost < Map[curr].cost) cost = Map[curr].cost;
 
-	if (Map[curr].type == 'T')
-	{
-		cost -= Map[curr].cost;
-	}
 
-	for (int i = 0; i < Map[curr].next.size(); i++)
-	{
+	//연결된 모든 방을 탐색한다.
+	for (int i = 0; i < Map[curr].next.size(); i++) {
+
 		int next = Map[curr].next[i];
-		if (Check(curr, cost, next) == false) continue;
+		if (Map[next].type == 'T' && cost - Map[next].cost < 0) continue;
 		if (Visit[next] == 1) continue;
-		
+
 		Visit[next] = 1;
 		Dfs(next, cost);
 		if (Flag == true) return;
@@ -71,18 +66,24 @@ void Dfs(int curr, int cost) {
 }
 
 void Clear() {
-	memset(Visit, 0, sizeof(Visit));
 	Flag = false;
+	memset(Visit, 0, sizeof(Visit));
 }
 
 int main() {
-	while (true)
-	{
+
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true) {
+
 		Input();
 		Visit[1] = 1;
 		Dfs(1, 0);
 		if (Flag == true) cout << "Yes" << '\n';
 		else cout << "No" << '\n';
 		Clear();
+
 	}
 }
