2b562046a95574fda421dbf21fe90888304cd8b4
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-19 18:49:42 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -1,50 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <queue>
-#include <map>
-#include<unordered_map>
-#include<unordered_set>
 #include <algorithm>
-#include <utility>
+#include <vector>
+#include <queue>
 #include <cstring>
 #include <string>
-#include <set>
-#include <sstream>
-#include <deque>
 using namespace std;
 
-int R, C, K;
+int N, M, K, Ans, Visit[6][6];
 char Map[6][6];
-int Visit[6][6];
-vector<int> Vector;
 
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
-int Ans;
 
 void Input() {
-	cin >> R >> C >> K;
-	for (int i = 1; i <= R; i++)
-		for (int j = 1; j <= C; j++)
+	cin >> N >> M >> K;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
 			cin >> Map[i][j];
+		}
+	}
 }
 
 void Dfs(int x, int y, int Count) {
-	if (x == 1 && y == C)
-	{
+
+	//cout << "x y : " << x << " , " << y << endl;
+	//cout << Count << endl << endl;
+
+	if (x == 1 && y == M) {
 		if (Count == K) Ans++;
+		//cout << Count << endl;
 		return;
 	}
 
-	for (int i = 0; i < 4; i++)
-	{
+	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
 		int ny = y + dy[i];
-		if (nx < 1 || ny < 1 || nx > R || ny > C) continue;
-		if (Map[nx][ny] == 'T') continue;
+		if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
 		if (Visit[nx][ny] == 1) continue;
+		if (Map[nx][ny] == 'T') continue;
 
 		Visit[nx][ny] = 1;
 		Dfs(nx, ny, Count + 1);
@@ -52,9 +45,16 @@ void Dfs(int x, int y, int Count) {
 	}
 }
 
+
 int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	Input();
-	Visit[R][1] = 1;
-	Dfs(R, 1, 1);
+	Visit[N][1] = 1;
+	Dfs(N, 1, 1);
 	cout << Ans;
+
 }
