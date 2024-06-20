83ae9a4194122c9f393e31d62301ce9eee1c63fe
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-20 18:17:48 +0900

    [Gold IV] Title: 무기 공학, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.cc
+++ 백준/Gold/18430. 무기 공학/무기 공학.cc
@@ -1,58 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <queue>
-#include <map>
-#include<unordered_map>
-#include<unordered_set>
 #include <algorithm>
-#include <utility>
+#include <vector>
+#include <queue>
 #include <cstring>
 #include <string>
-#include <set>
-#include <sstream>
-#include <deque>
 using namespace std;
 
-
-int N, M, Ans = 0;
+int N, M, Ans;
 int Map[5][5];
 int Visit[5][5];
-
-vector<pair<int, int>> Vector;
 
 int dx[4][2] = { {0, 1}, {0, -1}, {-1, 0}, {1, 0} };
 int dy[4][2] = { {-1, 0}, {-1, 0}, {0, 1}, {0, 1} };
 
+
 void Input() {
 	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			cin >> Map[i][j];
+		}
+	}
 }
 
-void Dfs(int Count, int Idx, int Sum, int G) {
-
-	if (Count == G)
-	{
+void Dfs(int Count, int Sum) {
+	if (Count == N * M) {
 		Ans = max(Ans, Sum);
 		return;
 	}
 
-	for (int i = Idx; i <= N * M; i++)
-	{
-		int x = i / M;
-		int y = i % M;
-		if (Visit[x][y] == 1) continue;
+	int x = Count / M;
+	int y = Count % M;
 
-		for (int d = 0; d < 4; d++)
-		{
+	if (Visit[x][y] == 0) {
+		for (int d = 0; d < 4; d++) {
 			int nx = x + dx[d][0];
 			int ny = y + dy[d][0];
 			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
 			if (Visit[nx][ny] == 1) continue;
-
 
 			int nnx = x + dx[d][1];
 			int nny = y + dy[d][1];
@@ -60,24 +45,18 @@ void Dfs(int Count, int Idx, int Sum, int G) {
 			if (Visit[nnx][nny] == 1) continue;
 
 
-			Visit[x][y] = 1;
-			Visit[nx][ny] = 1;
-			Visit[nnx][nny] = 1;
-
-			Dfs(Count + 1, i + 1, Sum + (2 * Map[x][y]) + Map[nx][ny] + Map[nnx][nny], G);
-
-			Visit[x][y] = 0;
-			Visit[nx][ny] = 0;
-			Visit[nnx][nny] = 0;
+			Visit[x][y] = Visit[nx][ny] = Visit[nnx][nny] = 1;
+			Dfs(Count + 1, Sum + (Map[nx][ny] + Map[nnx][nny] + 2 * Map[x][y]));
+			Visit[x][y] = Visit[nx][ny] = Visit[nnx][nny] = 0;
 		}
 	}
+
+	Dfs(Count + 1, Sum);
+
 }
 
 int main() {
 	Input();
-	for (int i = 1; i <= (N * M) / 3; i++)
-	{
-		Dfs(0, 0, 0, i);
-	}
+	Dfs(0, 0);
 	cout << Ans;
 }
