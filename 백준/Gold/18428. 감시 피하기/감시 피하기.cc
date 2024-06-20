057deb5cda27edf8dba2b7145e4349125753b18f
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-20 17:28:01 +0900

    [Gold V] Title: 감시 피하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.cc
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.cc
@@ -1,106 +1,77 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <queue>
 #include <cstring>
+#include <string>
 using namespace std;
 
 int N;
-char Map[100][100];
-int Visit[100][100];
-
+char Map[6][6];
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
-
-
-void Print() {
-	cout << "MAP : " << endl;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cout << Map[i][j] << ' ';
-		}
-		cout << endl;
-	}
-	cout << endl;
-
-	cout << "Visit : " << endl;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cout << Visit[i][j] << ' ';
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
+bool Flag = false;
 
 
 void Input() {
 	cin >> N;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			cin >> Map[i][j];
-}
-
-void Spread(int x, int y) {
-	Visit[x][y] = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		while (nx >= 0 && ny >= 0 && nx <= N && ny <= N && Map[nx][ny] != 'O')
-		{
-			Visit[nx][ny] = 1;
-			nx += dx[i];
-			ny += dy[i];
 		}
 	}
 }
 
-bool Check() {
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			if (Map[i][j] == 'T')
-				Spread(i, j);
-	//감시완료
+bool Spread() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (Map[i][j] == 'T') {
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			if (Map[i][j] == 'S' && Visit[i][j] == 1) 
-				return false;
-	//하나라도 잡힘
+				for (int d = 0; d < 4; d++) {
+					int px = i + dx[d];
+					int py = j + dy[d];
 
+					while (px >= 0 && py >= 0 && px < N && py < N && Map[px][py] != 'O') {
+						if (Map[px][py] == 'S') return false;
+						px += dx[d];
+						py += dy[d];
+					}
+				}
+			}
+		}
+	}
 	return true;
-	//모두 생존
 }
 
+
+//N*N 개 중 3개 고르기
 void Dfs(int Count, int Idx) {
-	if (Count == 3)
-	{
-		if (Check() == true)
-		{
-			cout << "YES" << endl;
-			exit(0);
+
+	if (Count == 3) {
+		if (Spread() == true) {
+			Flag = true;
 		}
-		memset(Visit, 0, sizeof(Visit));
 		return;
 	}
 
-	for (int i = Idx; i < N * N; i++)
-	{
-		int x = i / N;
-		int y = i % N;
-		if (Map[x][y] != 'X') continue;
 
-		Map[x][y] = 'O';
-		Dfs(Count + 1, i + 1);
-		Map[x][y] = 'X';
+	for (int i = Idx; i < N * N; i++) {
+		int nx = i / N;
+		int ny = i % N;
+
+		if (Map[nx][ny] == 'X') {
+			Map[nx][ny] = 'O';
+			Dfs(Count + 1, i);
+			if (Flag == true) return;
+			Map[nx][ny] = 'X';
+		}
 	}
 }
+
 
 int main() {
 	Input();
 	Dfs(0, 0);
-	cout << "NO" << endl;
+	if (Flag == true) cout << "YES" << endl;
+	else cout << "NO" << endl;
 }
