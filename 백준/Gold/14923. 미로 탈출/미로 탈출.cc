30936acf379bb88e4edd27304bac2a83bb6a3996
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-20 15:03:28 +0900

    [Gold IV] Title: 미로 탈출, Time: 200 ms, Memory: 13764 KB -BaekjoonHub

--- 백준/Gold/14923. 미로 탈출/미로 탈출.cc
+++ 백준/Gold/14923. 미로 탈출/미로 탈출.cc
@@ -1,112 +1,97 @@
 #include <iostream>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <queue>
-#include <map>
-#include<unordered_map>
-#include<unordered_set>
 #include <algorithm>
+#include <vector>
+#include <queue>
 #include <cstring>
 #include <string>
-#include <set>
-#include <sstream>
-#include <deque>
 using namespace std;
 
-int N, M, Map[1001][1001], Visit[1001][1001][2];
-// [0] = 지팡이 안 씀
-// [1] = 지팡이 씀
+int N, M;
+int Hx, Hy;
+int Ex, Ey;
+int Map[1001][1001];
+int Visit[1001][1001][2];
+//0 은 지팡이를 안쓴 상태로 진입
+//1 은 지팡이를 쓴 상태로 진입
 
-int Sx, Sy;
-int Gx, Gy;
 
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
 
-struct Data {
+struct Status {
 	int x;
 	int y;
 	int time;
-	bool used;
+	bool use;
 };
 
 void Input() {
-
 	cin >> N >> M;
-	cin >> Sx >> Sy;
-	cin >> Gx >> Gy;
-
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= M; j++)
+	cin >> Hx >> Hy;
+	cin >> Ex >> Ey;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
 			cin >> Map[i][j];
+		}
+	}
 }
 
-int Bfs(int x, int y) {
+int Bfs() {
 
-	queue<Data> Q;
-	Q.push({x, y, 0, false});
-	Visit[x][y][0] = 1;
-	//초기화
+	queue<Status> Q;
+	Visit[Hx][Hy][0] = 1;
+	Q.push({ Hx, Hy, 0, false });
 
-	while (!Q.empty())
-	{
+	while (!Q.empty()) {
 		int px = Q.front().x;
 		int py = Q.front().y;
 		int time = Q.front().time;
-		bool used = Q.front().used;
+		bool use = Q.front().use;
 		Q.pop();
 
-		//목표 도착
-		if (px == Gx && py == Gy)
-		{
+		if (px == Ex && py == Ey) {
 			return time;
 		}
 
-		for (int i = 0; i < 4; i++)
-		{
+
+		for (int i = 0; i < 4; i++) {
 			int nx = px + dx[i];
 			int ny = py + dy[i];
-			if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
 
+			if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
 			
-			//지팡이 사용 X
-			if (used == false)	
-			{
-				if (Map[nx][ny] == 1)
-				{
-					if (Visit[nx][ny][1] == 1) continue;
+			//다음 칸이 빈칸
+			if (Map[nx][ny] == 0) {
+				//지팡이 사용X
+				if (use == false && Visit[nx][ny][0] == 0) {
+					Visit[nx][ny][0] = 1;
+					Q.push({ nx, ny, time + 1, use });
+				}
+				//지팡이 사용O
+				if (use == true && Visit[nx][ny][1] == 0) {
+					Visit[nx][ny][1] = 1;
+					Q.push({ nx, ny, time + 1, use });
+				}
+			}
+			//다음 칸이 벽
+			//지팡이를 사용하지 않은 경우만 전진이 가능하다.
+			else {
+				if (use == false && Visit[nx][ny][1] == 0) {
 					Visit[nx][ny][1] = 1;
 					Q.push({ nx, ny, time + 1, true });
 				}
-
-				if (Map[nx][ny] == 0)
-				{
-					if (Visit[nx][ny][0] == 1) continue;
-					Visit[nx][ny][0] = 1;
-					Q.push({ nx, ny, time + 1, false });
-				}
-			}
-
-			//지팡이 사용 O
-			if (used == true)
-			{
-				if (Map[nx][ny] == 1) continue;
-				if (Visit[nx][ny][1] == 1) continue;
-				Visit[nx][ny][1] = 1;
-				Q.push({ nx, ny, time + 1, true });
 			}
 
 		}
-		
+
 	}
 
-	//도착 X
 	return -1;
 
 }
 
 int main() {
 	Input();
-	cout << Bfs(Sx, Sy);
+	int Result = Bfs();
+	cout << Result;
 }
