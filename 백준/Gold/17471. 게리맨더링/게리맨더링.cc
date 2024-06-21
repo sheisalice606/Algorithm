0cebaa1d84c40e8c6f66a79cc3479e23ca62de19
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-21 18:23:05 +0900

    [Gold III] Title: 게리맨더링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.cc
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.cc
@@ -1,166 +1,129 @@
 #include <iostream>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <queue>
-#include <map>
-#include<unordered_map>
-#include<unordered_set>
 #include <algorithm>
+#include <vector>
+#include <queue>
 #include <cstring>
+#include <map>
 #include <string>
-#include <set>
-#include <sstream>
-#include <deque>
 using namespace std;
 
-int N;
-int Map[11];
-int Visit[11];
-int Edge[11][11];
+int N, Visit[11], Person[11], Ans = 1e9;
+vector<int> Node[11];
+vector<int> TeamA, TeamB;
 
 
-vector<int> A;	//백트래킹 조합
-vector<int> B;	//A에 해당하지 않는 노드
+bool Check_TeamA() {
 
-
-int Visit_A[11];
-vector<int> Ans;
-
-
-void Input() {
-	cin >> N;
-	for (int i = 1; i <= N; i++) cin >> Map[i];
-	for (int i = 1; i <= N; i++)
-	{
-		int Temp; 
-		cin >> Temp;
-		for (int j = 1; j <= Temp; j++)
-		{
-			int Node;
-			cin >> Node;
-			Edge[i][Node] = 1;
-		}
-	}
-}
-
-//선거구의 인구 차이
-int Make_Ans() {
-	int Sum_A = 0, Sum_B = 0;
-	for (int i = 0; i < A.size(); i++)
-		Sum_A += Map[A[i]];
-	for (int i = 0; i < B.size(); i++)
-		Sum_B += Map[B[i]];
-	return abs(Sum_A - Sum_B);
-}
-
-//각 노드집합이 모두 인접한지 Check
-bool Bfs() {
-
-	//A
 	queue<int> Q;
-	Q.push(A[0]);
-	Visit[A[0]] = 1;
-	
-	while (!Q.empty())
-	{
+	int Temp[11] = { 0 };
+	Q.push(TeamA[0]);
+	Temp[TeamA[0]] = 1;
+	//삽입과 방문처리
+
+	while (!Q.empty()) {
 		int curr = Q.front();
 		Q.pop();
 
-		for (int i = 0; i < A.size(); i++)
-		{
-			int next = A[i];
-			if (curr == next) continue;				//본인 노드
-			if (Edge[curr][next] == 0) continue;	//이어지지 않은 경우
-			if (Visit[next] == 1) continue;			//이미 방문한 경우
+		for (int i = 0; i < Node[curr].size(); i++) {
+			int next = Node[curr][i];
+			if (Temp[next] == 1) continue;
+			if (Visit[next] == 0) continue;
+			//B팀에 속한 노드인 경우 전진불가
 
-			Visit[next] = 1;
+			Temp[next] = 1;
 			Q.push(next);
 		}
 	}
-
-	for (int i = 0; i < A.size(); i++)
-		if (Visit[A[i]] == 0)
-			return false;
-
-
-	//B
-	memset(Visit, 0, sizeof(Visit));
-	queue<int> Q2;
-	Q2.push(B[0]);
-	Visit[B[0]] = 1;
-
-	while (!Q2.empty())
-	{
-		int curr = Q2.front();
-		Q2.pop();
-
-		for (int i = 0; i < B.size(); i++)
-		{
-			int next = B[i];
-			if (curr == next) continue;				//본인 노드
-			if (Edge[curr][next] == 0) continue;	//이어지지 않은 경우
-			if (Visit[next] == 1) continue;			//이미 방문한 경우
-
-			Visit[next] = 1;
-			Q2.push(next);
-		}
+	//모든 연결처리
+	
+	for (int i = 0; i < TeamA.size(); i++) {
+		if (Temp[TeamA[i]] == 0) return false;
 	}
-
-	for (int i = 0; i < B.size(); i++)
-		if (Visit[B[i]] == 0)
-			return false;
-
-
 	return true;
 }
 
-//A,B 조합 생성
-void Dfs(int Count, int Idx, int Goal) {
+bool Check_TeamB() {
 
-	if (Count == Goal)
-	{
-		for (int i = 1; i <= N; i++)
-			if (Visit_A[i] == 0)
-				B.push_back(i);
+	queue<int> Q;
+	int Temp[11] = { 0 };
+	Q.push(TeamB[0]);
+	Temp[TeamB[0]] = 1;
+	//삽입과 방문처리
 
-		if (Bfs() == true)
-			Ans.push_back(Make_Ans());
-		
-		B.clear();
-		memset(Visit, 0, sizeof(Visit));
+	while (!Q.empty()) {
+		int curr = Q.front();
+		Q.pop();
+
+		for (int i = 0; i < Node[curr].size(); i++) {
+			int next = Node[curr][i];
+			if (Temp[next] == 1) continue;
+			if (Visit[next] == 1) continue;
+			//A팀에 속한 노드인 경우 전진불가
+
+			Temp[next] = 1;
+			Q.push(next);
+		}
+	}
+	//모든 연결처리
+
+	for (int i = 0; i < TeamB.size(); i++) {
+		if (Temp[TeamB[i]] == 0) return false;
+	}
+	return true;
+}
+
+int Make_Ans() {
+	int sum_A = 0, sum_B = 0;
+	for (int n : TeamA) sum_A += Person[n];
+	for (int n : TeamB) sum_B += Person[n];
+	return abs(sum_A - sum_B);
+}
+
+void Make_TeamB() {
+	for (int i = 1; i <= N; i++) {
+		if (Visit[i] == 0) TeamB.push_back(i);
+	}
+}
+
+void Dfs(int Count, int Idx, int G) {
+	//팀 구성이 완료된 경우
+	if (Count == G) {
+		Make_TeamB();
+		if (Check_TeamA() && Check_TeamB()) {
+			Ans = min(Ans, Make_Ans());
+		}
+		TeamB.clear();
 		return;
 	}
 
-	for (int i = Idx; i <= N; i++)
-	{
-		A.push_back(i);
-		Visit_A[i] = 1;
+	for (int i = Idx; i <= N; i++) {
+		Visit[i] = 1;
+		TeamA.push_back(i);
 
-		Dfs(Count + 1, i + 1, Goal);
+		Dfs(Count + 1, i + 1, G);
 
-		Visit_A[i] = 0;
-		A.pop_back();
+		TeamA.pop_back();
+		Visit[i] = 0;
 	}
-
 }
 
 int main() {
-
-	Input();
-	for (int i = 1; i <= N / 2; i++)
-	{
+	cin >> N;
+	for (int i = 1; i <= N; i++) cin >> Person[i];
+	for (int i = 1; i <= N; i++) {
+		//i번 영역에 인접한 영역의 수 = num
+		int num; 
+		cin >> num;
+		for (int j = 1; j <= num; j++) {
+			int temp; 
+			cin >> temp;
+			Node[i].push_back(temp);
+		}
+	}
+	
+	for (int i = 1; i <= N / 2; i++) {
 		Dfs(0, 1, i);
-		memset(Visit_A, 0, sizeof(Visit_A));
-	}
-	if (Ans.size() == 0)
-	{
-		cout << -1;
-	}
-	else
-	{
-		sort(Ans.begin(), Ans.end());
-		cout << Ans[0];
-	}
-
+	} 
+	if (Ans == 1e9) cout << -1;
+	else cout << Ans;
 }
