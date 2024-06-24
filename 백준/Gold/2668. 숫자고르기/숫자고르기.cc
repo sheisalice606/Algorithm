db8e540145be497e5dee2b16ba257e2fd66e6855
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-24 21:17:49 +0900

    [Gold V] Title: 숫자고르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.cc
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.cc
@@ -1,89 +1,71 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+#include <cstring>
 #include <string>
 #include <map>
-#include <cstring>
-#include <cmath>
-#include <set>
-#include <queue>
+#include <limits>
 #include <stack>
 using namespace std;
 
-int N;
-vector<int> Ans;
-vector<int> Map[101];
-vector<int> Temp;
-int Visit[101];
+int N, Map[101], Visit[101];
+
 bool Flag = false;
+vector<int> Temp, Ans;
 
+void Dfs(int curr, int start, int count) {
 
-void Dfs(int curr, int origin) {
-	//i는 1이다.
-	for (int i = 0; i < Map[curr].size(); i++)
-	{
-		int next = Map[curr][i];
-		//next : 다음 노드
+	int next = Map[curr];
 
-		//사이클 완성
-		if (next == origin)
-		{
-			Flag = true;
-			return;
-		}
-		
-		if (Visit[next] == 1) continue;
+	if (next == start && count > 0) {
+		Flag = true;
+		return;
+	}
 
+	if (Visit[next] == 0) {
 		Visit[next] = 1;
 		Temp.push_back(next);
-		Dfs(next, origin);
+		Dfs(next, start, count + 1);
 		Visit[next] = 0;
 	}
-}
 
+}
 
 int main() {
 	cin >> N;
-	for (int i = 1; i <= N; i++)
-	{
-		int num; cin >> num;
-		Map[i].push_back(num);
-		if (i == num)
-		{
+	for (int i = 1; i <= N; i++) {
+		cin >> Map[i];
+		if (i == Map[i]) {
 			Visit[i] = 1;
-			Ans.push_back(num);
+			Ans.push_back(i);
 		}
 	}
-	//데이터 저장
+	//입력
 
-	for (int i = 1; i <= N; i++)
-	{
-		if (Visit[i] == 0)
-		{
-			Visit[i] = 1;
-			Temp.push_back(i);
-			Dfs(i, i);
-			Visit[i] = 0;
+	for (int i = 1; i <= N; i++) {
+		if (Visit[i] == 1) continue;
 
-			if (Flag == true)
-				for (int n : Temp)
-				{
-					Visit[n] = 1;
-					Ans.push_back(n);
-					//사이클 확정 요소만 Visit
-				}
+		Visit[i] = 1;
+		Temp.push_back(i);
+		Dfs(i, i, 0);
+		Visit[i] = 0;
 
-			//초기화
-			Temp.clear();
-			Flag = false;
+		if (Flag == true) {
+			for (int n : Temp) {
+				Ans.push_back(n);
+				Visit[n] = 1;
+			}
 		}
+
+		Temp.clear();
+		Flag = false;
 	}
-	//사이클
+	//사이클 탐색 사이클이 2개 이상일 수 있다.
+	//사이클이 확정된 요소는 영구 방문처리하고, Ans에 삽입한다.
 
 	sort(Ans.begin(), Ans.end());
-	cout << Ans.size() << '\n';
-	for (int n : Ans)
-		cout << n << '\n';
-	//출력
+	cout << Ans.size() << endl;
+	for (int n : Ans) cout << n << endl;
 
 }
