89d5b2ef17fd9d8eef32363a44e2b5926a85b2db
sheisalice606 <77563691+sheisalice606@users.noreply.github.com>
2024-06-30 19:12:08 +0900

    [Gold IV] Title: 페그 솔리테어, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/9207. 페그 솔리테어/페그 솔리테어.cc
+++ 백준/Gold/9207. 페그 솔리테어/페그 솔리테어.cc
@@ -1,108 +1,84 @@
 #include <iostream>
-#include <vector>
-#include <stack>
-#include <cmath>
-#include <queue>
-#include <map>
-#include<unordered_map>
-#include<unordered_set>
 #include <algorithm>
+#include <vector>
+#include <queue>
 #include <cstring>
 #include <string>
-#include <set>
-#include <sstream>
-#include <deque>
+#include <map>
+#include <limits>
+#include <stack>
 using namespace std;
 
 int T;
+char Map[6][10];
+
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
-
-char Map[6][10];
 vector<pair<int, int>> Ans;
-//좌표 and 삭제 여부
+//핀의 수, 이동 횟수
 
 
-int Make_Ans() {
-	int Cnt = 0;
-	for (int i = 1; i <= 5; i++)
-		for (int j = 1; j <= 9; j++)
-			if (Map[i][j] == 'o')
-				Cnt++;
-	return Cnt;
-}
+void Dfs(int Count, char Curr[6][10]) {
 
-bool Cmp(pair<int, int> A, pair<int, int> B) {
-	if (A.first == B.first)
-		return A.second < B.second;
-	return A.first < B.first;
-}
+	for (int i = 1; i <= 5; i++) {
+		for (int j = 1; j <= 9; j++) {
+			if (Curr[i][j] == 'o') {
 
+				int x = i;
+				int y = j;
+				//움직일 핀의 위치
 
-void Dfs(int Count) {
-
-	for (int i = 1; i <= 5; i++)
-		for (int j = 1; j <= 9; j++)
-		{
-			if (Map[i][j] == 'o')
-			{
-				for (int d = 0; d < 4; d++)
-				{
-					int nx = i + dx[d];
-					int ny = j + dy[d];
+				for (int d = 0; d < 4; d++) {
+					int nx = x + dx[d];
+					int ny = y + dy[d];
 
 					int nnx = nx + dx[d];
 					int nny = ny + dy[d];
 
-					if (nx < 1 || ny < 1 || nx > 5 || ny > 9) continue;
-					if (Map[nx][ny] != 'o') continue;
-
-
 					if (nnx < 1 || nny < 1 || nnx > 5 || nny > 9) continue;
-					if (Map[nnx][nny] != '.') continue;
-					
+					if (Curr[nx][ny] != 'o' || Curr[nnx][nny] != '.') continue;
 
-					Map[i][j] = '.';							
-					Map[nx][ny] = '.';						
-					Map[nnx][nny] = 'o';	
+			
+					char Temp[6][10];
+					memcpy(Temp, Curr, sizeof(Temp));
 
-					Dfs(Count + 1);
-
-					Map[i][j] = 'o';
-					Map[nx][ny] = 'o';
-					Map[nnx][nny] = '.';
+					Temp[x][y] = Temp[nx][ny] = '.';
+					Temp[nnx][nny] = 'o';
+					Dfs(Count + 1, Temp);
 
 				}
+
 			}
 		}
+	}
 
-	Ans.push_back({ Make_Ans(), Count });
-	return;
+	//더이상 움직일 핀이 없을 떄
+	int Cnt = 0;
+	for (int i = 1; i <= 5; i++) {
+		for (int j = 1; j <= 9; j++) {
+			if (Curr[i][j] == 'o') Cnt++;
+		}
+	}
+	Ans.push_back({ Cnt, Count });
 
 }
 
 int main() {
-
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
 	cin >> T;
-	for (int t = 0; t < T; t++)
-	{
-		for (int i = 1; i <= 5; i++)
-		{
-			string str;
-			cin >> str;
-			for (int j = 0; j < 9; j++)
-				Map[i][j + 1] = str[j];
+	while (T--) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 9; j++) {
+				cin >> Map[i][j];
+			}
 		}
-		//초기화
-
-		Dfs(0);
-		sort(Ans.begin(), Ans.end(), Cmp);
+		
+		Dfs(0, Map);
+		
+		sort(Ans.begin(), Ans.end());
 		cout << Ans[0].first << ' ' << Ans[0].second << endl;
-		//출력
-
 		Ans.clear();
-		//리셋
 	}
-
 
 }
